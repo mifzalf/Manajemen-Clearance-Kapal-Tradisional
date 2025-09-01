1f19940a6b4360e2601989f1ed3f5f06a245b5a3
mifzal <ifzalxcalibur@gmail.com>
2025-09-01 09:08:07 +0700

    perjalanan

--- backend/controller/perjalananController.js
+++ backend/controller/perjalananController.js
@@ -15,65 +15,78 @@ const muatan = require("../model/muatanModel")
 const kategoriMuatan = require("../model/kategoriMuatanModel")
 
 const getPerjalananByFilter = async (req, res) => {
-    let { nama_kapal, kategori, tanggal_awal, tanggal_akhir, nama_muatan } = req.query
+    let { nama_kapal, kategori, tanggal_awal, tanggal_akhir, nama_muatan } = req.query;
+    
     try {
-        let whereClause = {};
+        let wherePerjalanan = {};
+        let whereKapal = {};
+        let whereKategoriMuatan = {};
 
-        if (tanggal_awal) {
-            whereClause.tanggal_berangkat = {
-                ...whereClause.tanggal_berangkat,
-                [Op.gte]: new Date(tanggal_awal),
+        if (nama_kapal) {
+            whereKapal.nama_kapal = { [Op.like]: `%${nama_kapal}%` };
+        }
+        
+        if (kategori) {
+            whereKategoriMuatan.status_kategori_muatan = { [Op.like]: `%${kategori}%` };
+        }
+        
+        if (nama_muatan) {
+            whereKategoriMuatan.nama_kategori_muatan = { [Op.like]: `%${nama_muatan}%` };
+        }
+
+        if (tanggal_awal && tanggal_akhir) {
+            wherePerjalanan.tanggal_berangkat = {
+                [Op.between]: [new Date(tanggal_awal), new Date(tanggal_akhir)]
+            };
+        } else if (tanggal_awal) {
+            wherePerjalanan.tanggal_berangkat = {
+                [Op.gte]: new Date(tanggal_awal)
+            };
+        } else if (tanggal_akhir) {
+            wherePerjalanan.tanggal_berangkat = {
+                [Op.lte]: new Date(tanggal_akhir)
             };
         }
 
-        if (tanggal_akhir) {
-            whereClause.tanggal_berangkat = {
-                ...whereClause.tanggal_berangkat,
-                [Op.lte]: new Date(tanggal_akhir),
-                ...whereClause.tanggal_berangkat,
-            };
-        }
         const datas = await perjalanan.findAll({
+            where: wherePerjalanan, 
             include: [
                 {
-                    model: kapal, attributes: ['nama_kapal', 'gt', 'nt', 'nomor_selar', 'tanda_selar', 'nomor_imo', 'call_sign'], include: [
+                    model: kapal,
+                    attributes: ['nama_kapal', 'gt', 'nt', 'nomor_selar', 'tanda_selar', 'nomor_imo', 'call_sign'],
+                    include: [
                         { model: jenis, attributes: ['nama_jenis'] },
                         { model: negara, as: "bendera", attributes: ['kode_negara'] }
-                    ], where: { nama_kapal: { [Op.like]: `%${nama_kapal || ""}%` } }
+                    ],
+                    where: whereKapal 
                 },
                 { model: spb, attributes: ['no_spb', 'no_spb_asal'] },
                 { model: nahkoda, attributes: ['nama_nahkoda'] },
                 { model: agen, attributes: ['nama_agen'] },
                 {
-                    model: muatan, attributes: ['jenis_perjalanan', 'satuan_muatan', 'jumlah_muatan'], include: [
+                    model: muatan,
+                    attributes: ['jenis_perjalanan', 'satuan_muatan', 'jumlah_muatan'],
+                    include: [
                         {
-                            model: kategoriMuatan, attributes: ['nama_kategori_muatan', 'status_kategori_muatan'], where: {
-                                status_kategori_muatan: { [Op.like]: `%${kategori || ""}%` },
-                                nama_kategori_muatan: { [Op.like]: `%${nama_muatan || ""}%` }
-                            }, required: true
+                            model: kategoriMuatan,
+                            attributes: ['nama_kategori_muatan', 'status_kategori_muatan'],
+                            where: whereKategoriMuatan, 
+                            required: Object.keys(whereKategoriMuatan).length > 0 
                         }
-                    ], required: true
+                    ],
+                    required: Object.keys(whereKategoriMuatan).length > 0
                 },
+                { model: kabupaten, as: "kedudukan_kapal", attributes: ['nama_kabupaten'] },
+                { model: kecamatan, as: "datang_dari", attributes: ['nama_kecamatan'] },
+                { model: kecamatan, as: "tempat_singgah", attributes: ['nama_kecamatan'] },
+                { model: kecamatan, as: "tujuan_akhir", attributes: ['nama_kecamatan'] },
             ],
-            where: {
-                [Op.and]: [
-                    {
-                        tanggal_berangkat: {
-                            [Op.gte]: new Date(tanggal_awal || "")
-                        }
-                    },
-                    {
-                        tanggal_berangkat: {
-                            [Op.lte]: new Date(tanggal_akhir || "")
-                        }
-                    },
-                ]
-            }
-        })
-        return res.status(200).json({ msg: "Berhasil mengambil data", datas })
+        });
+
+        return res.status(200).json({ msg: "Berhasil mengambil data", datas });
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({ msg: "terjadi kesalahan pada fungsi" })
+        console.log(error);
+        return res.status(500).json({ msg: "Terjadi kesalahan pada server" });
     }
 }
 
