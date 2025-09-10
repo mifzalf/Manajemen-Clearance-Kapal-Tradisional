17381c0a076e7637ff5fe3e178f3444068b3f42e
Ari-fauzi <arifauzi441@gmail.com>
2025-09-10 08:49:56 +0700

    memperbaiki middleware

--- backend/middleware/jwt.js
+++ backend/middleware/jwt.js
@@ -3,10 +3,10 @@ const jwt = require("jsonwebtoken")
 const verifyToken = (req, res, next) => {
     try {
         const token = req.header("Authorization")?.replace("Bearer ", "")
-        if (!token) return res.status(500).json({ msg: "tidak ada akses" })
+        if (!token) return res.status(401).json({ msg: "tidak ada akses" })
 
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) return res.status(500).json({ msg: "invalid / expired token" })
+            if (err) return res.status(401).json({ msg: "invalid / expired token" })
             req.user = decoded
             next()
         })
